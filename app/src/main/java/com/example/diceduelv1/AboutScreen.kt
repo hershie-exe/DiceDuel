ba4e25f20d1f180a6fda3be55817dad9905fdbe1
ADMIN <hirushidesilvasme@gmail.com>
2025-03-24 02:05:21 +0530

    made change in about screen

--- app/src/main/java/com/example/diceduelv1/AboutScreen.kt
+++ app/src/main/java/com/example/diceduelv1/AboutScreen.kt
@@ -72,7 +72,7 @@ fun AboutScreen(onBack: () -> Unit) {
                     Spacer(modifier = Modifier.height(16.dp))
 
                     PixelText(
-                        "I confirm that I understand what plagiarism is and have read and understood the section on Assessment Offences. This work is entirely my own.",
+                        "I confirm that I understand what plagiarism is and have read and understood the section on Assessment Offences in the Essential Information for Students. The work that I have submitted is entirely my own. Any work from other authors is duly referenced and acknowledged.",
                         fontSize = 14.sp
                     )
 
